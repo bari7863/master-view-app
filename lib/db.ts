3f959f530b7df7ec076d29e4e5172ea7c57143c8
bari <sutefani7863@gmail.com>
2026-03-19 09:43:09 +0900

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -5,15 +5,19 @@ declare global {
   var pgPool: Pool | undefined;
 }
 
-export const pool =
-  global.pgPool ||
-  new Pool({
-    host: process.env.POSTGRES_HOST,
-    port: Number(process.env.POSTGRES_PORT || 5432),
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-  });
+const poolConfig = process.env.DATABASE_URL
+  ? {
+      connectionString: process.env.DATABASE_URL,
+    }
+  : {
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT || 5432),
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+    };
+
+export const pool = global.pgPool || new Pool(poolConfig);
 
 if (process.env.NODE_ENV !== "production") {
   global.pgPool = pool;
